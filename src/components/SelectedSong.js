e80230678c8406ce2640482d8af6d1d9a724f59a
Ori Eliyahu <ori951753@gmail.com>
2020-12-25 09:12:59 +0200

    update the project

--- src/components/SelectedSong.js
+++ src/components/SelectedSong.js
@@ -3,12 +3,22 @@ import { connect } from 'react-redux';
 
 const SelectedSong = (props) => {
     if (props.selected)
-        return <div>{props.selected.title}</div>
-    return <div>{props.selected}</div>
+        return (
+            <div>
+                <h1>Selected Song:</h1>
+                <span><b>Title:</b> {props.selected.title}</span>
+                <br />
+                <span><b>Duration:</b> {props.selected.duration}</span>
+            </div>
+        );
+    return (
+        <div>
+            <h2>Select Song (;</h2>
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => {
-    // console.log("from selected song ", state);
     return { selected: state.selectedSong };
 };
 export default connect(mapStateToProps)(SelectedSong);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,13 @@ import { combineReducers } from 'redux';
 
 const songsReducer = (songsList = [], action) => {
     return [
-        { title: 'My Life', duration: '3:08' },
-        { title: 'My Love', duration: '3:48' },
-        { title: 'Ya Ghyab', duration: '4:08' },
-        { title: 'Policeman', duration: '2:08' },
-        { title: 'Salam', duration: '2:45' },
-        { title: 'Ori Niraa Tov', duration: '3:22' },
-        { title: 'Ori Niraa Tov 2', duration: '5:12' },
+        { title: 'Shine On', duration: '3:08' },
+        { title: "I Don't Wanna Live Forever ", duration: '3:48' },
+        { title: 'No Body No Crime', duration: '4:08' },
+        { title: 'For The Night', duration: '2:08' },
+        { title: 'Willow', duration: '2:45' },
+        { title: 'Tikitas', duration: '3:22' },
+        { title: 'Caramela', duration: '5:12' },
     ];
 };
 
